23e313f344effd22d7d5442def556dc38aff4ffb
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2020-12-08 12:31:20 +0300

    repeatedly buy stacks if we need more reagents than merchant can provide in a single purchase

--- SR13-ReagentBuy.lua
+++ SR13-ReagentBuy.lua
@@ -34,27 +34,36 @@ local function BuyReagentsForSelectedRecipe(cmd)
    end
    DevTools_Dump(need)
 
+   local buy_more_stacks
    local cost = 0
    for merchant_idx = 1, GetMerchantNumItems() do
       local link = GetMerchantItemLink(merchant_idx)
       local item_id = link and link:match("|Hitem:(%d+)")
       if item_id then
          item_id = item_id + 0
-         local need = need[item_id]
-         if need then
+         local buy_amount = need[item_id]
+         if buy_amount then
             local name, texture, price, quantity, numAvailable, isPurchasable, isUsable, extendedCost = GetMerchantItemInfo(merchant_idx)
             print(name, extendedCost)
             local maxStack = GetMerchantItemMaxStack(merchant_idx)
+            if buy_amount > maxStack then
+               buy_amount = maxStack
+               buy_more_stacks = true
+            end
             -- TODO: numAvailable, maxStack, extendedCost
             if not extendedCost or extendedCost == 0 then
-               cost = cost + (price / quantity) * need
-               print("BuyMerchantItem", merchant_idx, need)
-               BuyMerchantItem(merchant_idx, need)
+               cost = cost + (price / quantity) * buy_amount
+               print("BuyMerchantItem", merchant_idx, buy_amount)
+               BuyMerchantItem(merchant_idx, buy_amount)
             end
          end
       end
    end
    print(cost)
+
+   if buy_more_stacks then
+      C_Timer.After(0.2, function() return BuyReagentsForSelectedRecipe(cmd) end)
+   end
 end
 _G.BuyReagentsForSelectedRecipe = BuyReagentsForSelectedRecipe
 a_env.register_slash("BuyReagents", { "/breg" }, BuyReagentsForSelectedRecipe)
